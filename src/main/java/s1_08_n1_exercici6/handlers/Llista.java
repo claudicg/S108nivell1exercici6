c9ac6a641475f65b34f3f841a091ad51556b430f
Claudio Cordero <cordero.guiza@gmail.com>
2024-03-10 11:49:25 +0100

    First commit

---
+++ src/main/java/s1_08_n1_exercici6/handlers/Llista.java
@@ -0,0 +1,21 @@
+package s1_08_n1_exercici6.handlers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Llista {
+	 
+	public void order() {
+		
+		List<Object> llista = new ArrayList<>();
+		llista.add("Casa");
+		llista.add(5);
+		llista.add("Finestra");
+		llista.add("Porta");
+		llista.add(7);
+		
+		Collections.sort(llista, (s1, s2) -> s1.toString().length() - s2.toString().length());
+		llista.forEach(System.out :: println);
+	}	
+}
---
+++ src/main/java/s1_08_n1_exercici6/main/LlistaMain.java
@@ -0,0 +1,12 @@
+package s1_08_n1_exercici6.main;
+
+import s1_08_n1_exercici6.handlers.Llista;
+
+public class LlistaMain {
+
+	public static void main(String[] args) {
+		
+		Llista demo = new Llista();
+		demo.order();
+	}
+}
